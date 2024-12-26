e698cf00cab22577ca7676f97fb5d8005911fd8f
suleei <suleei@qq.com>
2024-12-26 15:33:44 +0800

    update use ThreadLocal to store user information, avoid redundant get user information, add removing in response return

--- src/main/java/com/oft/fittools/global/ResponseResult.java
+++ src/main/java/com/oft/fittools/global/ResponseResult.java
@@ -8,18 +8,21 @@ public class ResponseResult<T> {
     private T data;
 
     public static <T> ResponseResult<T> success(){
+        UserContextHolder.clearUser();
         ResponseResult responseResult = new ResponseResult();
         responseResult.message="success";
         return responseResult;
     }
 
     public static <T> ResponseResult<T> success(T data){
+        UserContextHolder.clearUser();
         ResponseResult responseResult = new ResponseResult();
         responseResult.data=data;
         responseResult.message="success";
         return responseResult;
     }
     public static <T> ResponseResult<T> fail(String message){
+        UserContextHolder.clearUser();
         ResponseResult responseResult = new ResponseResult();
         responseResult.message=message;
         return responseResult;
